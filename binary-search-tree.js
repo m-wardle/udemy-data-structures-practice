e3a8b7d9b852a8e488064b5316efb43aa71cce76
Matt Wardle <mwardle.22@gmail.com>
2020-02-08 09:42:59 -0500

    Add boolean 'contains' search method

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -20,14 +20,41 @@ class BinarySearchTree {
       }
     }
   }
+
+  contains(value) {
+    if (typeof value !== "number") {
+      return false;
+    }
+    if (value === this.value) {
+      return true;
+    } else if (value < this.value) {
+      if (!this.left) {
+        return false
+      } else {
+        return this.left.contains(value)
+      }
+    } else if (value > this.value) {
+      if (!this.right) {
+        return false;
+      } else {
+        return this.right.contains(value);
+      }
+    }
+  }
 }
 
 const bst = new BinarySearchTree(50);
 
 bst.insert(30);
+bst.insert(70);
+bst.insert(100);
 bst.insert(60);
-bst.insert(55);
-bst.insert(10);
+bst.insert(59);
+bst.insert(20);
 bst.insert(45);
+bst.insert(35);
+bst.insert(85);
+bst.insert(105);
+bst.insert(10);
 
-console.log(bst);
+console.log(bst.contains(30));
